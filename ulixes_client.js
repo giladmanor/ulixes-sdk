8d2993986e85d3cb46e536c38cb68b0063d0c658
Gilad Manor <giladmanor01@gmail.com>
2014-01-01 03:05:32 +0200

    javascript client cleanup

--- ulixes_client.js
+++ ulixes_client.js
@@ -1,87 +1,79 @@
-$(document).ready(function() {
-	console.log("------------");
-});
+var Ulixes = function(token, server, dataHandler) {
+	var self = this;
+	this.token = token;
+	this.server = server;
 
-function ulixes_token(token) {
-	$.ajax({
-		type : "POST",
-		url : ulixes_server + "api/use_token",
-		dataType : "jsonp",
-		data : {
+	if (( typeof dataHandler === "undefined") || !dataHandler) {
+		this.dataHandler = defaultDataHandler;
+	}
+
+	function use_token(token) {
+		self.send("use_token", {
 			t : token,
 			with_info : true
-		},
-		success : function(data) {
-			console.log("success::useToken");
-			updateData(data);
-		}
-	});
-}
+		});
+	}
 
-function ulixes_get() {
-	$.ajax({
-		type : "POST",
-		dataType : "jsonp",
-		url : ulixes_server + "api/get",
-		data : { },
-		success : function(data) {
-			updateData(data);
-		}
-	});
-}
 
-function ulixes_set(code, value) {
-	$.ajax({
-		type : "POST",
-		dataType : "jsonp",
-		url : ulixes_server + "api/set",
-		data : {
+	this.get = function() {
+		self.send("get", {});
+	};
+
+	this.set = function(code, value) {
+		self.send("set", {
 			code : code,
 			value : value,
 			with_info : true
-		},
-		success : function(data) {
-			updateData(data);
+		});
+	};
+
+	var send = function(action, data) {
+		$.ajax({
+			type : "POST",
+			dataType : "jsonp",
+			url : self.server + "api/" + action,
+			data : data,
+			success : function(data) {
+				self.dataHandler(data);
+			}
+		});
+	};
+
+	var defaultDataHandler = function(data) {
+
+		function plotBadges(data) {
+			//clean badges
+			$(".ulixes-badge").each(function() {
+				$(this).text("");
+			});
+			data.badges.map(function(k) {
+				$(".ulixes-badge-" + k.placeholder).append("<img src='" + ulixes_server + k.icon + "' title='" + k.name + "'>");
+			});
 		}
-	});
-}
 
-//////////////////////////////////////////////////////////////////////////////////////////
+		function plotScores(data) {
+			data.scores.map(function(k) {
+				$(".ulixes-score-" + k.code).text(k.value);
+			});
+		}
 
-function updateData(data) {
-	plotNode(data);
-	plotBadges(data);
-	plotScores(data);
-	plotNotifications(data);
-}
+		function plotNotifications(data) {
+			$('.ulixes-announcements').text("");
+			$('.ulixes-announcements-count').text(data.announcements ? data.announcements.length : 0);
+			for (var i = 0; i < data.announcements.length; i += 1) {
+				$('.ulixes-announcements').append(data.announcements[i].message + "<br/>");
+			}
+			if (data.announcements.length == 0) {
+				$('.ulixes-announcements').html("No New Notifications");
+			}
+		}
 
-function plotNode(cluster) {
-	$(".ulixes-node").text(cluster.label);
-}
+		plotBadges(data);
+		plotScores(data);
+		plotNotifications(data);
+	};
 
-function plotBadges(data) {
-	//clean badges
-	$(".ulixes-badge").each(function(){
-		$(this).text("");
-	});
-	data.badges.map(function(k) {
-		$(".ulixes-badge-" + k.placeholder).append("<img src='" + ulixes_server + k.icon + "' title='" + k.name + "'>");
-	});
-}
+	use_token(token);
 
-function plotScores(data) {
-	data.scores.map(function(k) {
-		$(".ulixes-score-" + k.code).text(k.value);
-	});
-}
+};
 
-function plotNotifications(data) {
-	$('.ulixes-announcements').text("");
-	$('.ulixes-announcements-count').text( data.announcements ? data.announcements.length : 0);
-	for (var i = 0; i < data.announcements.length; i += 1) {
-		$('.ulixes-announcements').append(data.announcements[i].message + "<br/>");
-	}
-	if (data.announcements.length == 0) {
-		$('.ulixes-announcements').html("No New Notifications");
-	}
-}
