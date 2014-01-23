49185eb8d5c4b2ff049b4dfd7c9e02773442fc32
Gilad Manor <giladmanor01@gmail.com>
2014-01-24 01:35:03 +0200

    test view update

--- ulixes_tester.php
+++ ulixes_tester.php
@@ -12,10 +12,7 @@ include_once ("ulixes_client.php");
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://code.jquery.com/jquery.js"></script>
 		<script src="ulixes_client.js"></script>
-		<script>
-		
-		
-		</script>
+		<script></script>
 		<!-- <script src="http://d3js.org/d3.v3.min.js"></script> -->
 		<script src="d3.min.js"></script>
 	</head>
@@ -24,14 +21,17 @@ include_once ("ulixes_client.php");
 		<?php
 		include_once ("ulixes_controle_bar.php");
 		?>
-		<div class="pull-right">
+		<div class="container">
 			<div class="chartGoesHere"></div>
-			
-			<div class="text-muted" style="margin-top: -237px;margin-left: 178px;">
-				<strong>User Vector</strong>
+			<div class="actionChartGoesHere" style="margin-top: -383px;margin-left: 65px;"></div>
+			<div class="text-muted" style="margin-top: -232px;margin-left: 140px;">
+				<h2>User Vector</h2>
 			</div>
-			<div class="actionChartGoesHere" style="margin-top: -163px;margin-left: 65px;"></div>
-			<div class="alert alert-info" style="margin-top: 20px;">
+			<div style="margin-top: 0px;margin-left: 178px;">
+				<strong class="ulixes-node"></strong>
+			</div>
+
+			<div class="col-xs-5 alert alert-info" style="margin-top: 167px;">
 				<form class="form-inline" role="form" onsubmit="cnt.set($('#code').val(),$('#value').val());return false;">
 					<div class="form-group ">
 						<input name="code" id="code" type="text" class="form-control" placeholder="code" value="a">
@@ -44,23 +44,20 @@ include_once ("ulixes_client.php");
 					</button>
 				</form>
 			</div>
-		</div>
-		<div class="row " style="margin-left: 1px">
-			
-			<div class=" col-xs-2">
+			<div class=" col-xs-4" style=" float: right; margin-top: -195px; ">
 				<div class="panel panel-info">
 					<div class="panel-heading">
-						Location on Graph
+						Notifications
 					</div>
-					<div class="panel-body ulixes-node">
-						
+					<div class="panel-body" style=" overflow: auto; height: 250px; ">
+						<ul class="list-unstyled ulixes-announcements" style="overflow: auto">
+							nothing
+						</ul>
 					</div>
 
 				</div>
 			</div>
-			
-			
-			<div class=" col-xs-2">
+			<div class=" col-xs-2" style=" float: right; margin-top: -195px; ">
 				<div class="panel panel-info">
 					<div class="panel-heading">
 						Badges
@@ -73,21 +70,12 @@ include_once ("ulixes_client.php");
 
 				</div>
 			</div>
-			<div class=" col-xs-6">
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						Notifications
-					</div>
-					<div class="panel-body">
-						<ul class="list-unstyled ulixes-announcements" style="overflow: auto">
-							nothing
-						</ul>
-					</div>
+		</div>
+		<div class="" style="margin-left: 1px">
 
-				</div>
-			</div>
 			
 			
+
 		</div>
 
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -95,6 +83,6 @@ include_once ("ulixes_client.php");
 	</body>
 	<script src="ulixes_tester.js"></script>
 	<script>
-		var cnt=new Ulixes('<?php echo ULIXES_SERVER?>','<?php echo $token?>',testerDataHandler);
+var cnt=new Ulixes('<?php echo ULIXES_SERVER?>','<?php echo $token?>',testerDataHandler);
 	</script>
 </html>
