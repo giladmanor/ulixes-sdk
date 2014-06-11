3432245ac64363a8d24c56db30630a5586cec1cf
Gilad <giladmanor01@gmail.com>
2014-06-11 19:50:51 +0300

    update tracker

--- ulixes_tracker.js
+++ ulixes_tracker.js
@@ -1,8 +1,10 @@
 var Ulixes = function(server, auth, dataHandler) {
-	
-	this.open = function(uid){
+
+	this.open = function(uid, d) {
+		console.log("ULIXES UUID: ",uid);
 		self.auth.uuid = uid;
-		get(self.auth);
+		$.extend(d || {}, self.auth);
+		self.get(d);
 	};
 
 	this.get = function(d) {
@@ -13,7 +15,7 @@ var Ulixes = function(server, auth, dataHandler) {
 		send("set", {
 			code : code,
 			value : value,
-			with_info : true
+			with_info : false
 		});
 	};
 
@@ -25,62 +27,92 @@ var Ulixes = function(server, auth, dataHandler) {
 	};
 
 	var send = function(action, data) {
+		$.extend(data, self.auth);
 		$.ajax({
 			type : "POST",
-			dataType : "jsonp",
 			url : self.server + "api/" + action,
 			data : data,
-			success : function(data) {
-				self.dataHandler(data, self.server);
+			success : function(res) {
+				console.log("Ulixes response: ",res);
+				self.dataHandler(res, self.server);
+			},
+			error : function(err){
+				console.log("Ulixes ERROR: ",err);
 			}
 		});
 	};
-	
-	var resolveServer = function(server){
-		if(server.indexOf("http")!=-1){
+
+	var resolveServer = function(server) {
+		if (server.indexOf("http") != -1) {
 			return server;
-		}else{
-			return "http://"+server;
+		} else {
+			return "http://" + server;
 		}
 	};
 
 	var defaultDataHandler = function(data) {
-
+		
 		function plotBadges(data) {
 			//clean badges
 			$(".ulixes-badge").each(function() {
 				$(this).text("");
 			});
 			data.badges.map(function(k) {
-				$(".ulixes-badge-" + k.placeholder).append("<img height='100' width='100' src='" + ulixes_server + k.icon + "' title='" + k.name + "'>");
+				$(".ulixes-badge-" + k.placeholder).append("<img height='40' width='40' src='http://bozz.wikibrains.com/" + k.icon + "' title='" + k.name + "'>");
 			});
 		}
 
 		function plotScores(data) {
 			data.scores.map(function(k) {
-				$(".ulixes-score-" + k.code).text(k.value);
+				$(".ulixes-score-card" + k.code).text(k.value);
 			});
 		}
 
-		function plotNotifications(data) {
-			$('.ulixes-announcements').text("");
-			$('.ulixes-announcements-count').text(data.announcements ? data.announcements.length : 0);
-			for (var i = 0; i < data.announcements.length; i += 1) {
-				$('.ulixes-announcements').append(data.announcements[i].message + "<br/>");
-			}
-			if (data.announcements.length == 0) {
-				$('.ulixes-announcements').html("No New Notifications");
-			}
+		function message(id, data) {
+			return '<li class="list-group"><a href="#" style="float:right" onclick="ulixes.read(' + id + ');return false;">X</a><p class="list-group-item-text">' + data.message + '</p></li>';
+		}
+		
+		function pop_message(id, data) {
+			return '<a href="#" style="float:right" onclick="ulixes.read(' + id + ');return false;">X</a><br/><p ><strong>' + (data.title || "") +
+			 '</strong></p><p >' + (data.message) + '</p>';
+		}
+		
+		function video_message(id, data) {
+			return '<a href="#" style="float:right" onclick="ulixes.read(' + id + ');return false;">X</a><p style="margin-top: -10px;"><strong>' + (data.message) +
+			 '</strong></p><br/><p ><iframe style="margin-left: -21px;" width="479" height="260" src="'+(data.video)+'" frameborder="0" allowfullscreen></iframe></p>';
 		}
 
-
+		function plotNotifications(data) {
+			
+			$('.ulixes-announcements').text("");
+			$('.ulixesNotifications').hide();
+			
+			$('#ulixes-popup-messages').text("");
+			$('.ulixes-popup').hide();
+			
+			$('.ulixes-announcements-count').text(data.announcements ? data.announcements.length : 0);
+			data.announcements.map(function(k) {
+				console.log("m: ", k);
+				if(k.channel==="popup"){
+					if(k.format==="video"){
+						$('#ulixes-popup-messages').html(video_message(k.id, k.data));
+					}else{
+						$('#ulixes-popup-messages').html(pop_message(k.id, k.data));
+					}
+					$('.ulixes-popup').show();
+				}else{
+					$('.ulixes-announcements').append(message(k.id, k.data));
+					$('.ulixesNotifications').show();	
+				}
+			});
+		}
 		//console.log("defaultDataHandler");
 		plotBadges(data);
 		plotScores(data);
 		plotNotifications(data);
 	};
-	
-	// INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT 
+
+	// INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT INIT
 	var self = this;
 	this.auth = auth;
 	this.server = resolveServer(server);
@@ -90,10 +122,11 @@ var Ulixes = function(server, auth, dataHandler) {
 	} else {
 		this.dataHandler = dataHandler;
 	}
-	
+
 };
 
-var ulixes = new Ulixes("localhost:3000", {
-	a_id : 2,
-	k : "433VSapgiZ2jb033NJjpilRNmAoUlVNH6cuhnnpXca91Vrsfq5"
+var ulixes = new Ulixes("http://bozz.wikibrains.com/", {
+	a_id : 3,
+	k : "aaBzqa5vKxQPInhDKpLfKzgkuWzQvnj1Ma9GXLQq9uglcUYa0I"
 });
+
